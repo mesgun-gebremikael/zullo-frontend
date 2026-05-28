d8ca9f0e9678ae9a9624efe9a194830bb4a9f72c
Mesgun <Mesgun@MESGUN->
2026-05-28 18:04:56 +0200

    Add onboarding data model

---
+++ lib/onboarding/onboarding_data.dart
@@ -0,0 +1,11 @@
+class OnboardingData {
+  String displayName = '';
+  DateTime? birthDate;
+  String gender = '';
+  String interestedIn = '';
+  String intention = '';
+  int distanceKm = 50;
+  String religion = 'Private';
+  String bio = '';
+  List<String> photoUrls = [];
+}
